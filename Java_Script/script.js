bf0663cca7280ed6c0eb6ec5ba745793ebb54920
dudaz-dev <dudaz.dev@gmail.com>
2024-10-18 09:05:24 -0300

    retirei comentários

--- Java_Script/script.js
+++ Java_Script/script.js
@@ -1,35 +1,29 @@
-// Certifica-se de que o DOM foi carregado completamente
 document.addEventListener("DOMContentLoaded", () => {
-  // Seleciona os elementos necessários
+
   const tecladoForca = document.querySelector(".tecladoForca");
   const imagem = document.querySelector(".imagemForca img");
 
-  // Função de inicialização
+
   function init() {
-    // Define a imagem inicial da forca
+
     imagem.src = `img/forca.png`;
 
-    // Gera as teclas (botões)
     geradorDeTeclas();
   }
 
-  // Função para criar as teclas (botões) de A a Z
   function geradorDeTeclas() {
-    tecladoForca.textContent = ""; // Limpa o conteúdo antes de adicionar as letras
+    tecladoForca.textContent = "";
 
-    // Loop para criar os botões de 'A' a 'Z'
-    for (let i = 97; i < 123; i++) { // 'a' é 97 no código ASCII e 'z' é 122
+    for (let i = 97; i < 123; i++) {
       const botao = document.createElement("button");
-      const letra = String.fromCharCode(i).toUpperCase(); // Converte o código ASCII para a letra correspondente
+      const letra = String.fromCharCode(i).toUpperCase();
       botao.textContent = letra;
-      botao.classList.add('letra-botao'); // Adiciona uma classe para estilizar (opcional)
+      botao.classList.add('letra-botao');
 
-      // Adiciona o botão ao contêiner de teclas
       tecladoForca.appendChild(botao);
     }
   }
 
-  // Inicia o jogo ao carregar a página
   init();
 });
 
